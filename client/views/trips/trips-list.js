1fc6a7102770ae6a2e684155418ba716a3440ddf
Chyld Medford <chyld.medford@gmail.com>
2015-05-05 15:42:44 -0700

    add trip

---
+++ client/views/trips/trips-list.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('roadtrip')
+.controller('TripsListCtrl', function(){
+});
---
+++ client/views/trips/trips-show.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('roadtrip')
+.controller('TripsShowCtrl', function(){
+});
